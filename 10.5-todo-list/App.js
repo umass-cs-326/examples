45f1298749d4af330a63c1e07067e23308c0618e
Tim Richards <richards@cs.umass.edu>
2024-03-19 17:42:52 +0000

    Refactor UI component mounting and add comments

--- 10.5-todo-list/App.js
+++ 10.5-todo-list/App.js
@@ -7,10 +7,18 @@ import Storage from './Storage.js';
 const App = mountPoint => {
   const storage = Storage();
 
+  // Restore the tasks from local storage or use the provided default tasks.
   const tasks = storage.restore([
     Task('Learn about Web Components'),
     Task('Go for a walk'),
   ]);
+
+  // Get the mount point.
+  const mountDom = document.getElementById(mountPoint);
+
+  // Used to reference the dom element for this UI component.
+  let dom = document.createElement('div');
+  dom.id = 'App';
 
   const addTask = (tasks, task) => {
     mount([...tasks, Task(task)]);
@@ -23,12 +31,16 @@ const App = mountPoint => {
   };
 
   const mount = tasks => {
+    // Build the UI components.
     const textInput = TextInput(addTask, tasks);
     const taskList = TaskList(deleteTask, tasks);
     const todoList = TodoList(textInput, taskList);
-    const mount = document.getElementById(mountPoint);
-    mount.innerHTML = '';
-    mount.appendChild(todoList);
+    dom.innerHTML = '';
+    dom.appendChild(todoList);
+
+    // Add the UI component dom to the mount point.
+    mountDom.innerHTML = '';
+    mountDom.appendChild(dom);
     storage.save(tasks);
   };
 
